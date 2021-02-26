a97a6847de7896fd0de75247f43a46dd52bbfd94
박종현 <parkjonghyun@bagjonghyeon-ui-MacBookPro.local>
2021-02-26 19:20:39 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [value, setValue] = useState('');
+  useEffect(() => {
+    console.log(value);
+  }, [value]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>와우</p>
+      <p>와우</p>
+      <p>와우</p>
+      <p>와우</p>
+      <p>와우</p>
     </div>
   );
 }
